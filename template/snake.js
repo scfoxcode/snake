75ac54002c49f0af58813e273fc1ab637048a70f
stephen.surtees <scfoxcode@hotmail.com>
2019-11-29 23:17:54 +0000

    Small cleanup

--- template/snake.js
+++ template/snake.js
@@ -1,19 +1,27 @@
-
-// Function to export which contains the game
+// Function to export that contains the game
 function Snake(containerId) {
+    var self = this;
     this.welcome = 'Hello, lets play Snake!';
     this.gameOver = false;
+    this.containerId = containerId;
+    this.ctx = null;
 
     this.init(); // Initialise the game
 
     this.gameLoop = setInterval(function() { // Game loop
-        this.update();
-        this.render();
+        self.update();
+        self.render();
     }, 200);
 }
 
 Snake.prototype.init = function() {
     console.log(this.welcome);
+
+    // Initialise the canvas and add it to the dom
+    var canvas = document.createElement('canvas');
+    var container = document.getElementById(this.containerId);
+    container.appendChild(canvas);
+    this.ctx = canvas.getContext('2d');
 };
 
 Snake.prototype.update = function() {
