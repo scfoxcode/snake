22685a9ca42f36b8ce75cfc9ed5ebaa4c322aaaa
Stephen J Surtees <stephen.publicmail@gmail.com>
2019-12-07 21:23:32 +0000

    Moving snake head

--- complete/snake.js
+++ complete/snake.js
@@ -11,12 +11,24 @@ function Snake(containerId) {
         numRows: 32,
         gridGap: 2
     };
+    this.state = {
+        snake: {
+            head: {
+                x: Math.floor(this.gridProps.numColumns / 2),
+                y: Math.floor(this.gridProps.numRows / 2),
+                next: null
+            },
+            color: '#F00',
+            bodyColor: '#F88',
+            direction: 'right'
+        }
+    }
 
     this.init(); // Initialise the game
 
     this.gameLoop = setInterval(function() { // Game loop
-        self.update();
-        self.render(self.ctx);
+        self.update(self.state);
+        self.render(self.ctx, self.state);
     }, 200);
 }
 
@@ -33,22 +45,47 @@ Snake.prototype.init = function() {
     this.ctx = canvas.getContext('2d');
 };
 
-Snake.prototype.update = function() {
+Snake.prototype.update = function(state) {
+    const { snake } = state;
+
+    // Determine new position for snake head
+    let pos = {x: snake.head.x, y: snake.head.y};
+    switch (snake.direction) {
+        case 'left': pos.x -= 1; break;
+        case 'right': pos.x += 1; break;
+        case 'up': pos.y -= 1; break;
+        case 'down': pos.y += 1; break;
+    }
+
+    snake.head.x = pos.x;
+    snake.head.y = pos.y;
+
 };
 
 // BEGIN RENDER FUNCTIONS
-Snake.prototype.drawRect = function(ctx, x, y, width, height, colour) {
+Snake.prototype.drawRect = function(ctx, x, y, width, height, color) {
     ctx.beginPath();
     ctx.rect(x, y, width, height);
-    ctx.fillStyle = colour;
+    ctx.fillStyle = color;
     ctx.fill();
 };
 
-Snake.prototype.drawSquare = function(ctx, x, y, size, colour) {
-    this.drawRect(ctx, x, y, size, size, colour);
+Snake.prototype.drawSquare = function(ctx, x, y, size, color) {
+    this.drawRect(ctx, x, y, size, size, color);
 };
 
-Snake.prototype.render = function(ctx) {
+Snake.prototype.drawCell = function(ctx, x, y, color) {
+    const { blockSize, gridGap } = this.gridProps;
+    this.drawSquare(
+        ctx,
+        x * blockSize + x * gridGap,
+        y * blockSize + y * gridGap,
+        blockSize,
+        color,
+    );
+}
+
+Snake.prototype.render = function(ctx, state) {
     const { blockSize, numColumns, numRows, gridGap } = this.gridProps;
 
     // Clear canvas
@@ -57,14 +94,12 @@ Snake.prototype.render = function(ctx) {
     // Draw grid
     for (let i=0; i<numColumns; i++) {
         for (let j=0; j<numRows; j++) {
-            this.drawSquare(
-                ctx,
-                i * blockSize + i * gridGap,
-                j * blockSize + j * gridGap,
-                blockSize,
-                '#DDE',
-            );
+            this.drawCell(ctx, i, j, '#DDE');
         }
     }
+
+    // Draw snake
+    const { snake } = state;
+    this.drawCell(ctx, snake.head.x, snake.head.y, snake.color);
 };
 // END RENDER FUNCTIONS
