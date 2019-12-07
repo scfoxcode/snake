fdd0ffdd41ac48edff87c67cf44f0e2be132c1d2
Stephen J Surtees <stephen.publicmail@gmail.com>
2019-12-07 20:23:35 +0000

    Finished basic template

--- template/snake.js
+++ template/snake.js
@@ -1,25 +1,38 @@
 // Function to export that contains the game
 function Snake(containerId) {
-    var self = this;
+    const self = this;
     this.welcome = 'Hello, lets play Snake!';
     this.gameOver = false;
     this.containerId = containerId;
     this.ctx = null;
+    this.gridConsts = {
+        blockSize: 16,
+        numColumns: 32,
+        numRows: 32,
+        gridGap: 2
+    };
 
     this.init(); // Initialise the game
 
     this.gameLoop = setInterval(function() { // Game loop
         self.update();
-        self.render();
+        self.render(self.ctx);
     }, 200);
 }
 
 Snake.prototype.init = function() {
     console.log(this.welcome);
+    const { blockSize, numColumns, numRows, gridGap } = this.gridConsts;
 
     // Initialise the canvas and add it to the dom
-    var canvas = document.createElement('canvas');
-    var container = document.getElementById(this.containerId);
+    const canvas = document.createElement('canvas');
+    canvas.width = 
+        blockSize * numColumns +
+        (numColumns - 1) * gridGap;
+    canvas.height = 
+        blockSize * numRows +
+        (numRows - 1) * gridGap;
+        const container = document.getElementById(this.containerId);
     container.appendChild(canvas);
     this.ctx = canvas.getContext('2d');
 };
@@ -27,6 +40,35 @@ Snake.prototype.init = function() {
 Snake.prototype.update = function() {
 };
 
-Snake.prototype.render = function() {
+// BEGIN RENDER FUNCTIONS
+Snake.prototype.drawRect = function(ctx, x, y, width, height, colour) {
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.fillStyle = colour;
+    ctx.fill();
 };
 
+Snake.prototype.drawSquare = function(ctx, x, y, size, colour) {
+    this.drawRect(ctx, x, y, size, size, colour);
+};
+
+Snake.prototype.render = function(ctx) {
+    const { blockSize, numColumns, numRows, gridGap } = this.gridConsts;
+
+    // Clear canvas
+    this.drawRect(ctx, 0, 0, ctx.canvas.width, ctx.canvas.height, '#EEE');
+
+    // Draw grid
+    for (let i=0; i<numColumns; i++) {
+        for (let j=0; j<numRows; j++) {
+            this.drawSquare(
+                ctx,
+                i * blockSize + i * gridGap,
+                j * blockSize + j * gridGap,
+                blockSize,
+                '#DDE',
+            );
+        }
+    }
+};
+// END RENDER FUNCTIONS
