d1c91627280b4d95e2b3a66b435565425f985017
Stephen J Surtees <stephen.publicmail@gmail.com>
2019-12-07 23:10:55 +0000

    Snake and food work; Need refactor

--- complete/snake.js
+++ complete/snake.js
@@ -2,7 +2,6 @@
 function Snake(containerId) {
     const self = this;
     this.welcome = 'Hello, lets play Snake!';
-    this.gameOver = false;
     this.containerId = containerId;
     this.ctx = null;
     this.gridProps = {
@@ -11,19 +10,7 @@ function Snake(containerId) {
         numRows: 32,
         gridGap: 2
     };
-    this.state = {
-        snake: {
-            head: {
-                x: Math.floor(this.gridProps.numColumns / 2),
-                y: Math.floor(this.gridProps.numRows / 2),
-                next: null
-            },
-            color: '#F00',
-            bodyColor: '#F88',
-            direction: 'right',
-            pendingDirection: 'right'
-        }
-    }
+    this.state = {}; // Set in init
 
     this.init(self.state); // Initialise the game
 
@@ -35,6 +22,7 @@ function Snake(containerId) {
 
 Snake.prototype.init = function(state) {
     console.log(this.welcome);
+    state.gameOver = false;
     const { blockSize, numColumns, numRows, gridGap } = this.gridProps;
 
     // Initialise the canvas and add it to the dom
@@ -45,27 +33,70 @@ Snake.prototype.init = function(state) {
     container.appendChild(canvas);
     this.ctx = canvas.getContext('2d');
 
+    // Initialise the snake state
+    state.snake = {
+        head: {
+            x: Math.floor(this.gridProps.numColumns / 2),
+            y: Math.floor(this.gridProps.numRows / 2),
+            next: null
+        },
+        color: '#F00',
+        bodyColor: '#F88',
+        direction: 'right',
+        pendingDirection: 'right'
+    };
+    state.snake.head.uidPos = this.uidPos({
+        x: state.snake.head.x,
+        y: state.snake.head.y
+    });
+    
+    // Snake should be called game, and state.snake should be it's own object
+
+    // Set food state
+    state.food = [];
+
     // Add keyboard events
-    window.addEventListener('keypress', function (e) {  
+    window.addEventListener('keydown', function (e) {  
         const { snake } = state;
-        if (e.keyCode === 37 || e.keyCode === 97) { // left
+        if (e.key === 'ArrowLeft' || e.key === 'a') {
             if (snake.direction !== 'right') {
                 snake.pendingDirection = 'left';
             }
-        } else if (e.keyCode === 39 || e.keyCode === 100) { // right
+        } else if (e.key === 'ArrowRight' || e.key === 'd') {
             if (snake.direction !== 'left') {
                 snake.pendingDirection = 'right';
             }
-        } else if (e.keyCode === 38 || e.keyCode === 119) { // up
+        } else if (e.key === 'ArrowUp' || e.key === 'w') {
             if (snake.direction !== 'down') {
                 snake.pendingDirection = 'up';
             }
-        } else if (e.keyCode === 40 || e.keyCode === 115) {
+        } else if (e.key === 'ArrowDown' || e.key === 's') {
             if (snake.direction !== 'up') {
                 snake.pendingDirection = 'down';
             }    
         }
     });
+};
+
+Snake.prototype.uidPos = function(pos) {
+    return ('' + pos.x + '_' + pos.y);
+};
+
+Snake.prototype.posFromUid = function(uid) {
+    const values = uid.split('_');
+    return {x: parseInt(values[0]), y: parseInt(values[1])};
+};
+
+Snake.prototype.mapSnakeNodes = function(snake, f) {
+    const results = [];
+    let node = snake.head;
+    let index = 0;
+    while (node) {
+        results.push(f(node, index));
+        node = node.next;
+        ++index;
+    }
+    return results;
 };
 
 Snake.prototype.move = function(snake, pos, preserveTail) {
@@ -84,16 +115,43 @@ Snake.prototype.move = function(snake, pos, preserveTail) {
     }
 
     // Delete old tail
-    let node = snake.head.next;
+    let node = snake.head;
     while (node && node.next) {
         if (!node.next.next) {
             node.next = null;
         }
+        node = node.next;
     }
+    // Probably makes sense to store next and prev, makes this much simpler
+}
+
+// This is expensive, minimise calls
+Snake.prototype.listFreeCells = function() {
+    const { numColumns, numRows } = this.gridProps;
+    const { snake, food } = this.state;
+    const takenCells = [];
+
+    // List cells snake occupies
+    this.mapSnakeNodes(snake, node => {
+        takenCells.push(node.uidPos);
+    });
+
+    // Determine free cells
+    const freeCells = [];
+    for (let i=0; i<numColumns; i++) {
+        for (let j=0; j<numRows; j++) {
+            const id = this.uidPos({x: i, y: j});
+            if (!takenCells.includes(id)) {
+                freeCells.push(id);
+            }
+        }
+    }
+
+    return freeCells;
 }
 
 Snake.prototype.update = function(state) {
-    const { snake } = state;
+    const { snake, food } = state;
 
     // Apply direction provided from event handlers
     snake.direction = snake.pendingDirection;
@@ -109,10 +167,27 @@ Snake.prototype.update = function(state) {
 
     // Check if position is legal
 
-    const foodEaten = false;
+    let foodEaten = false;
+    const foodUid = this.uidPos(pos);
+    for (let i=0; i<food.length; i++) {
+        if (food[i].uid === foodUid) {
+            foodEaten = true;
+            food.splice(i, 1);
+            console.log("this happened");
+            break;
+        }
+    }
+
     // If it is legal, set new head position
     this.move(snake, pos, foodEaten);
-
+    if (!food.length) {
+        const freeCells = this.listFreeCells();
+        const pick = Math.floor(Math.random() * (freeCells.length -1));
+        const foodPos = this.posFromUid(freeCells[pick]);
+        state.food.push({pos: foodPos, uid: freeCells[pick]});
+    }
+    
+    
 };
 
 // BEGIN RENDER FUNCTIONS
@@ -151,8 +226,17 @@ Snake.prototype.render = function(ctx, state) {
         }
     }
 
+    // Draw food
+    state.food.forEach(food => {
+        this.drawCell(ctx, food.pos.x, food.pos.y, '#080');
+    });
+
     // Draw snake
     const { snake } = state;
-    this.drawCell(ctx, snake.head.x, snake.head.y, snake.color);
+    this.mapSnakeNodes(snake, (node, index) => {
+        const color = index === 0 ? snake.color : snake.bodyColor;
+        this.drawCell(ctx, node.x, node.y, color);
+    });
+    
 };
 // END RENDER FUNCTIONS
